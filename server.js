3084518bba291fcfdd7a6a46ba232e33e734f874
rcclarkjr <rcclarkjr@yahoo.com>
2026-01-29 09:20:24 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -338,7 +338,7 @@ async function callOpenAIAPI(
     : messages;
 
   const requestBody = {
-    model: "gpt-4-turbo",
+    model: "gpt-4-turbo-2024-04-09",
     messages: finalMessages,
     max_tokens: maxTokens,
     temperature
